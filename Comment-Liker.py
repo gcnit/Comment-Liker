cfaaab93829115166fb8cc644f10c338369e4b34
Gaurav <gaurav01101993@gmail.com>
2014-10-26 01:15:23 +0530

    v1.0

--- Comment-Liker.py
+++ Comment-Liker.py
@@ -1,16 +1,18 @@
 import requests
 import json
 import time
-import urllib2
-import facebook
-def main():
-    Token = raw_input("\nGo to https://developers.facebook.com/tools/explorer ,\nclick on get access token,\ncheck on all the permissions in basic and extended permissions \nand paste the access token here\n")
-    post_id = raw_input("\nEnter the post id:\n")
+
+Token = raw_input("\nGo to https://developers.facebook.com/tools/explorer ,\nclick on get access token,\ncheck on all the permissions in basic and extended permissions \nand paste the access token here\n")
+post_id = raw_input("\nEnter the post id:\n")
+self_id = json.loads(requests.get("https://graph.facebook.com/me?access_token=" + Token).text)['id']
+
+def like():
     post = "https://graph.facebook.com/" + post_id + "/comments?access_token=" + Token;
     response = requests.get(post).text
     jsondata = json.loads(response)
     for comment in jsondata['data']:
-        if str(comment['from']['id'])!="539395822858113":
-            requests.get("https://graph.facebook.com/"+str(comment['id'])+"/likes?access_token="+Token+"&POST")
+        if str(comment['from']['id']) != str(self_id):
+            requests.get("https://graph.facebook.com/" + str(comment['id']) + "/likes?access_token=" + Token + "&POST")
+            
 if __name__ == "__main__":
-    main()
+    like()
